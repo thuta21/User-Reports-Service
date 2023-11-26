14cb4c8082860b9ff0711218bb26e750e1b03a65
Thuta Min Thway <thutaminthway2001@gmail.com>
2023-11-26 20:33:46 +0630

    add crud for user module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+    return user;
+  }
+
+  @Get()
+  async findAllUsers(@Query('email') email: string) {
+    const user = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+    return user;
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.delete(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
